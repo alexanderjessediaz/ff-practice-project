0f93478dc2627cbf5347e512614ef8776d376e31
AJ Diaz <ajdiaz@trouthousetech.com>
2022-08-16 15:49:13 -0600

    minor edits to login screen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,112 +1,97 @@
 import Head from 'next/head';
 import {FormEvent, useState} from 'react';
-
 import {auth, firestore} from '@/firebaseApp/init';
 import {signInWithEmailAndPassword} from 'firebase/auth';
 import {doc, getDoc} from 'firebase/firestore';
 
 import {useRouter} from 'next/router';
 import {Spinner} from '@/components/shared/spinner';
+import { url } from 'inspector';
 
 const Login = () => {
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
-  const [submitting, setSubmitting] = useState(false);
-
-  const router = useRouter();
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    setSubmitting(true);
-
-    const docRef = doc(firestore, 'admin', emailAddress);
-    const docSnap = await getDoc(docRef);
-
-    //Check if the User who is signing in is an Admin
-    if (docSnap.exists()) {
-      signInWithEmailAndPassword(auth, emailAddress, password)
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          router.push('/');
-        })
-        .catch((error) => {
-          setMessage('Email and/or password is invalid');
-          setSubmitting(false);
-        });
-    } else {
-      setMessage('Email and/or password is invalid');
-      setSubmitting(false);
-    }
-  };
 
   return (
     <>
       <Head>
-        <title>Sign In | vABi Admin</title>
+        <title>Sign In</title>
       </Head>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <img className="mx-auto h-12 w-auto" src="/logo.jpg" alt="" />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to Admin Portal</h2>
-          </div>
-          <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  onChange={(event) => {
-                    setEmailAddress(event.currentTarget.value);
-                  }}
-                  value={emailAddress}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md  focus:z-10 sm:text-sm"
-                  placeholder="Email address"
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  onChange={(event) => {
-                    setPassword(event.currentTarget.value);
-                  }}
-                  value={password}
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
-
+        <div className="h-screen grid content-center flex justify-center  py-12 px-4 sm:px-6 lg:px-8">
+          
+          <div className="max-w-md w-full space-y-8">
+            
             <div>
-              <button
-                type="submit"
-                disabled={submitting}
-                className="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-app-dark-purple  focus:outline-none focus:ring-2 focus:ring-offset-2 gap-2">
-                Sign in
-                {submitting && <Spinner className="text-white" />}
-              </button>
-              <p className="text-center text-red-600 my-2">{message}</p>
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             </div>
-          </form>
+            <form className="mt-8 space-y-6" action="#" method="POST">
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-sm">
+                  <a href="#" className="font-medium text-gray-600 hover:text-green-700">
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign in
+                </button>
+              </div>
+              <div>
+                New to 
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
     </>
   );
 };
